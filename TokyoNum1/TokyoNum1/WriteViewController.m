c09a9eed2179871fe856f440c7f903f6711e9593
ngoctamtl <ngoctam.9k@gmail.com>
2014-01-16 16:38:50 +0700

    First commit

---
+++ TokyoNum1/TokyoNum1/WriteViewController.m
@@ -0,0 +1,38 @@
+//
+//  WriteViewController.m
+//  TokyoNum1
+//
+//  Created by PhamNgocTam on 1/16/14.
+//  Copyright (c) 2014 PhamNgocTam. All rights reserved.
+//
+
+#import "WriteViewController.h"
+
+@interface WriteViewController ()
+
+@end
+
+@implementation WriteViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
